3f45cefb1722947fd625fcb9600fde04ea0c7080
Gumemura <gumemura123@gmail.com>
2021-03-27 00:07:30 -0300

    unit assets chenge

---
+++ Scripts/MapGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapGenerator : MonoBehaviour
+{
+	public Grid gamegrid;
+
+	public TileBase blockTile;
+	public TileBase greenTile;
+
+	public GameObject forest;
+
+	private Tilemap floorTilemap;
+	private Tilemap gridTilemap;
+	private Tilemap blockTilemap;
+
+	private Vector3Int floorOrigin;
+	private Vector3Int checkingCell;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	floorTilemap = gamegrid.transform.Find("Floor").GetComponent<Tilemap>();
+    	gridTilemap = gamegrid.transform.Find("Grid").GetComponent<Tilemap>();
+    	blockTilemap = gamegrid.transform.Find("Blocks").GetComponent<Tilemap>();
+
+    	floorOrigin = floorTilemap.origin;
+
+        floorTilemap.CompressBounds();
+
+        for(int i = 0; i < floorTilemap.size.x; i++){
+        	for(int c = 0; c > -floorTilemap.size.y; c--){
+        		checkingCell = new Vector3Int(i, c, floorOrigin.z);
+        		if(floorTilemap.GetTile(checkingCell) == null){
+        			blockTilemap.SetTile(checkingCell, greenTile);
+        			Instantiate(forest, gamegrid.CellToWorld(checkingCell) + new Vector3(0, gamegrid.cellSize.y/2, 0), Quaternion.identity, this.transform);
+        		}
+        	}
+        }
+    }
+}
---
+++ Scripts/StaticObjZCalculator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class StaticObjZCalculator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y * .01f);
+    }
+}
